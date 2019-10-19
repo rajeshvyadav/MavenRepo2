35e126d600614b37e0342078f6b7a3ccbb2e2c28
rajeshvyadav <rajeshv.yadav@gmail.com>
2019-10-19 14:38:47 +0530

    gitupload

---
+++ src/test/java/com/seleniumautomation/selenium/maventest.java
@@ -0,0 +1,13 @@
+package com.seleniumautomation.selenium;
+
+import org.testng.annotations.Test;
+
+public class maventest {
+
+	@Test
+	public void testmaven(){
+		
+		System.out.println("welcome to maven world");
+	}
+	
+}
